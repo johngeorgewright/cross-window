8e425b4fff2e8a15b664a464a94ce1fc1c6dedd6
John Wright <jrite@protonmail.com>
2024-01-22 18:30:28 +0000

    perf: no need to update init listeners

--- src/state.mjs
+++ src/state.mjs
@@ -2,10 +2,10 @@ const STORAGE_KEY = 'cw'
 
 let state = localStorage.getItem(STORAGE_KEY) ?? ''
 
-const listeners = []
-
-onUpdateState((value) => { state = value })
-onUpdateState((value) => localStorage.setItem(STORAGE_KEY, value))
+const listeners = [
+  (value) => { state = value },
+  (value) => localStorage.setItem(STORAGE_KEY, value),
+]
 
 addEventListener('storage', (event) => {
   if (event.key === STORAGE_KEY) updateState(event.newValue)
